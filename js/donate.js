0812e5e5132005c68d1044eb24870ded5d4ef3a1
ShahidaTanju <shahidasharmin450@gmail.com>
2024-09-23 22:55:49 +0600

    donate

---
+++ js/donate.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-donate').addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('donate button clicked')
+
+    const amountInput = document.getElementById('amount-input').value;
+    console.log(amountInput);
+
+
+
+})
