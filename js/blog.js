8f0c57096e869486aee26755079609755e73264f
ShahidaTanju <shahidasharmin450@gmail.com>
2024-09-24 20:52:50 +0600

    button

---
+++ js/blog.js
@@ -0,0 +1,9 @@
+document.getElementById('blog-btn').addEventListener('click', function () {
+
+    window.location.href = '../blog.html'
+});
+
+const back = document.getElementById('home-btn')
+back.addEventListener('click', function () {
+    window.location.href = '../index.html'
+})
--- js/donate.js
+++ js/donate.js
@@ -1,10 +1,15 @@
-document.getElementById('btn-donate').addEventListener('click', function (event) {
+document.getElementById('btn-donate-1').addEventListener('click', function (event) {
     event.preventDefault();
     console.log('donate button clicked')
 
-    const amountInput = document.getElementById('amount-input').value;
-    console.log(amountInput);
+    const amountInput1 = document.getElementById('amount-input-1').innerText;
+    console.log(amountInput1);
 
-
+    if (isNaN.Number(amountInput1) || amountInput1 < 0) {
+        alert('Please enter a valid amount');
+    }
+    else {
+        document.getElementById('my_modal_3').style.display = 'block';
+    }
 
 })
