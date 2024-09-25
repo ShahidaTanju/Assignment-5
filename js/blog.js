69698490372f71b2037861a1ba6b51195a17c459
ShahidaTanju <shahidasharmin450@gmail.com>
2024-09-25 11:49:25 +0600

    button exchange

--- js/blog.js
+++ js/blog.js
@@ -1,9 +1,12 @@
+// blog
 document.getElementById('blog-btn').addEventListener('click', function () {
 
-    window.location.href = './home.html'
+    window.location.href = '/blog.html'
 });
 
 const back = document.getElementById('home-btn')
 back.addEventListener('click', function () {
-    window.location.href = './index.html'
+    window.location.href = '/index.html'
 });
+
+
